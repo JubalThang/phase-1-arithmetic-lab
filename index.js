c05979e820fc18d111c889e8f0d7514f49fd68d6
jubalthang <jubalthang@gamil.com>
2021-07-08 22:00:48 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 8;
+let num2 = 8;
+const multiply = (num2 * num1) - 2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 18
+const num4 = 7
+const mod = num3 % num4
+
+const max = Math.ceil(19.6)
